25ce8bbe22310a036f6bb3328ddd15396c8724d0
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-08-18 22:24:45 -0300

    Tarea 17.0

---
+++ Modulo 1 - Front End Developer/Clase 17/Practica/script.js
@@ -0,0 +1,51 @@
+// const nombres = ['pepe', 'maria', 'pedro', 'juan', 'maria', 'maria']
+
+
+// while(nombres.includes('maria')){
+//     let posicionMaria = nombres.indexOf('maria')
+//     nombres.splice(posicionMaria, 1)
+// }
+
+const nombres = ['Juan','María','Pedro','Juan','Sofía','Luis','María','Ana','Pedro','Juan','Sofía','Carlos','Luis','Ana','María','Pedro','Sofía','Juan','Luis','Ana'];
+
+let nombre = 'Juan'
+nombreAElimimar(nombres, nombre)
+console.log(`1° Listado:
+    ${nombres}
+    se elimino el nombre: ${nombre}`)
+
+nombre = 'Carlos'
+nameToDelete(nombres,nombre)
+console.log(`2° Listado:
+    ${nombres}
+    se elimino el nombre: ${nombre}`)
+// for (let i = 0; i < nombres.length; i++) {
+//     console.log(`  ${nombres[i]}`);
+// }
+
+
+function nameToDelete(nombres,nombre){
+    for (let i = 0; i < nombres.length; i++) {
+        if (nombres[i] === nombre) {
+            nombres.splice(nombres.indexOf(nombre),1)
+        }
+    }
+    return (nombres)
+}
+
+function nombreAElimimar(nombres,nombre) {
+    // for (const nombre  of nombres) {
+    //     console.log(nombre);
+    //     if (nameToDelete === nombre) {
+    //         nombres.splice(nombres.indexOf(nombre),1)
+    //     }
+    //     /*Se tiene un problema al usar for of para eliminar un elemento del arreglo
+    //     cuando a se elimina el elemento siguiente toma su lucgar y no es analizado 
+    //     por la condicion if */
+    // }
+    while (nombres.includes(nombre)) {
+        nombres.splice(nombres.indexOf(nombre),1)
+    }
+    return (nombres)
+}
+
