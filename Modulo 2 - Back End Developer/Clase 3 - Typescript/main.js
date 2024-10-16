fae532d8723b3eedaa6502257ab7ec0bdb2d6e40
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-10-16 10:51:11 -0300

    Intro TypeScript

---
+++ Modulo 2 - Back End Developer/Clase 3 - Typescript/main.js
@@ -0,0 +1,6 @@
+// let nombre1 = '0'
+// nombre1= 'pepe'
+// definicion de datos explicito
+var nombre = 'pepe';
+var edad = 25;
+console.log(nombre);
---
+++ Modulo 2 - Back End Developer/Clase 3 - Typescript/main.ts
@@ -0,0 +1,9 @@
+// let nombre1 = '0'
+// nombre1= 'pepe'
+// definicion de datos explicito
+let nombre : string = 'pepe'
+let edad : number = 25
+
+console.log(nombre)
+
+
