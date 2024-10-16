a81057b9341112db496472ec88abfc3c6f55069f
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-10-16 11:58:49 -0300

    Clase 3

--- Modulo 2 - Back End Developer/Clase 3 - Typescript/main.js
+++ Modulo 2 - Back End Developer/Clase 3 - Typescript/main.js
@@ -4,3 +4,48 @@
 var nombre = 'pepe';
 var edad = 25;
 console.log(nombre);
+var statusEmai = null;
+var mostrarSttus = function () {
+    console.log(statusEmai);
+};
+setTimeout(function () {
+    statusEmai = true;
+    mostrarSttus();
+}, 1000);
+mostrarSttus();
+var calcularIva = function (precio) {
+    return precio * 0.21;
+};
+var suma = function (a, b) { return a + b; };
+var resultado = suma(1, 1);
+function doSomething() {
+    alert('Algo');
+}
+function buscarAlgo() {
+    var resultado;
+    return resultado;
+}
+console.log(doSomething());
+console.log(calcularIva(10));
+// const persona : {nombre:string, edad:number, id:number} = {
+//   nombre: 'pepe',
+//   edad: 25,
+//   id: 1
+// }
+var persona = {
+    nombre: 'pepe',
+    edad: 25,
+    id: 1
+};
+// const saludarPersona = (persona : {nombre:string, edad:number, id:number}) : void => {
+//   console.log('Hola ' + persona.nombre);
+// }
+var saludarPersona = function (persona) {
+    console.log('Hola ' + persona.nombre);
+};
+saludarPersona(persona);
+var resta = function (_a) {
+    var numMay = _a.numMay, numMen = _a.numMen;
+    return numMay - numMen;
+};
+resta({ numMay: 1, numMen: 2 });
--- Modulo 2 - Back End Developer/Clase 3 - Typescript/main.ts
+++ Modulo 2 - Back End Developer/Clase 3 - Typescript/main.ts
@@ -6,4 +6,70 @@ let edad : number = 25
 
 console.log(nombre)
 
+let statusEmai : null | boolean = null 
 
+const mostrarSttus = () => {
+  console.log(statusEmai);
+  
+}
+setTimeout(
+  () => {
+    statusEmai = true
+    mostrarSttus()
+  },
+  1000
+)
+
+mostrarSttus()
+
+const calcularIva = (precio : number) : number => {
+  return precio * 0.21
+}
+
+const suma = (a: number, b: number) : number => a + b;
+
+let resultado : number = suma(1,1)
+
+function doSomething () : void {
+  alert('Algo')
+}
+function buscarAlgo() : string | undefined {
+  let resultado : string | undefined
+  return resultado
+}
+
+console.log(doSomething());
+console.log(calcularIva(10));
+
+interface Persona {
+  nombre: string;
+  edad:number;
+  id:number
+}
+
+// const persona : {nombre:string, edad:number, id:number} = {
+//   nombre: 'pepe',
+//   edad: 25,
+//   id: 1
+// }
+const persona : Persona = {
+  nombre: 'pepe',
+  edad: 25,
+  id: 1
+}
+
+// const saludarPersona = (persona : {nombre:string, edad:number, id:number}) : void => {
+//   console.log('Hola ' + persona.nombre);
+// }
+
+const saludarPersona = (persona : Persona) : void => {
+  console.log('Hola ' + persona.nombre);
+}
+
+saludarPersona(persona)
+
+const resta = ({numMay, numMen} : {numMay: number, numMen: number}) : number => {
+    return numMay - numMen
+}
+
+resta ({numMay: 1, numMen:2})
